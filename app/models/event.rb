a51d8ef1abd9f0f7a8539a082dde8e0389a16198
Stan van de Poll <swvandepoll@gmail.com>
2019-01-10 19:34:14 +0100

    Completed Event Attendance, no extra credit

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
   has_many :invitations
   has_many :attendees, :through => :invitations
+
+  scope :past, -> { where("date <= ?", Time.now) }
+  scope :upcoming, -> { where("date >= ?", Time.now) }
 end
